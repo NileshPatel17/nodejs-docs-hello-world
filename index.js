00851bc6324634925e10684292423a832b6f2ebf
Nilesh Patel <nilesh.nvs@hotmail.com>
2020-05-27 09:00:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hola 2.1!");
+    response.end("Hola 2.2!");
 });
 
-const port = process.env.PORT || 1337;
+const port = 1337;
 server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
